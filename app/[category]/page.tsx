d6793d6dbcf316239c34c824ab6b28da9cff68a1
Bebe Jane <bjornthief@gmail.com>
2023-11-06 17:14:44 +0100

    fix

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -1,13 +1,13 @@
 'use server'
 
 import s from './page.module.scss'
-import { AllPoliticByCategoryDocument, AllPoliticCategoriesDocument, PoliticDocument } from "@graphql";
+import { AllPoliticByCategoryDocument, AllPoliticCategoriesDocument } from "@graphql";
 import { apiQuery } from "@lib/client";
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 export async function generateStaticParams() {
-  const { allPoliticCategories } = await apiQuery<AllPoliticCategoriesQuery, AllPoliticCategoriesQueryVariables>(AllPoliticCategoriesDocument, { generateTags: true });
+  const { allPoliticCategories } = await apiQuery<AllPoliticCategoriesQuery, AllPoliticCategoriesQueryVariables>(AllPoliticCategoriesDocument);
   return allPoliticCategories.map(({ slug: category }) => ({ category }))
 }
 
@@ -19,13 +19,19 @@ export default async function Page({ params }: { params: { category: string } })
 
   return (
     <section className={s.container}>
-      <ul>
-        {politicCategory?._allReferencingPolitics.map(({ title, slug, category }, idx) =>
-          <li key={idx}>
-            <Link href={`/${category.slug}/${slug}`}>{title}</Link>
-          </li>
-        )}
-      </ul>
+      {politicCategory?._allReferencingPolitics.length > 0 ?
+        <ul>
+          {politicCategory?._allReferencingPolitics.map(({ title, slug, category }, idx) =>
+            <li key={idx}>
+              <Link href={`/${category.slug}/${slug}`}>{title}</Link>
+            </li>
+          )}
+        </ul>
+        :
+        <p>
+          Det finnsinga poster i den här kategorin
+        </p>
+      }
     </section>
   );
 }

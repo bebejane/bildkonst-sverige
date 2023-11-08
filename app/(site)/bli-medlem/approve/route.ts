474ae8bef2f98536c0318218c120a027d99494bb
Bebe Jane <bjornthief@gmail.com>
2023-11-08 23:16:18 +0100

    fix

--- app/(site)/bli-medlem/approve/route.ts
+++ app/(site)/bli-medlem/approve/route.ts
@@ -12,10 +12,12 @@ export default function POST(req: Request) {
   return basicAuth(req, async (req) => {
     try {
       const { id } = (await req.formData()).get('entity') as any;
+      console.log(id)
       const record = await client.items.find(id);
-
+      console.log(record)
       if (!record)
         return NextResponse.json({ success: false, message: 'Record not found' });
+
 
       if (record.approved)
         console.log('send email to: ', record.email)
@@ -23,6 +25,7 @@ export default function POST(req: Request) {
       return NextResponse.json({ success: true });
 
     } catch (e) {
+      console.log(e.message)
       return NextResponse.json({ success: false, error: e.message });
     }
   });

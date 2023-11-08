1afbe0d73c4c9d6b9de40497936de06e07a79858
Bebe Jane <bjornthief@gmail.com>
2023-11-08 11:39:09 +0100

    orange scroll

--- components/common/OrangePower.tsx
+++ components/common/OrangePower.tsx
@@ -1,9 +1,33 @@
 'use client'
 
 import s from './OrangePower.module.scss'
+import { useScrollInfo } from 'dato-nextjs-utils/hooks'
+import { useState } from 'react';
+import { useScrollData } from "scroll-data-hook";
 
-export default async function OrangePower({ }: {}) {
+export default function OrangePower({ }: {}) {
+
+  const [active, setActive] = useState(false)
+
+  const {
+    scrolling,
+    time,
+    speed,
+    direction,
+    position,
+    relativeDistance,
+    totalDistance
+  } = useScrollData({
+    onScrollStart: () => setActive(true),
+    onScrollEnd: () => setActive(false)
+  });
+
+  const height = Math.floor((speed.y / 10000) * 100)
+  const styles = {
+    top: scrolling ? direction.y === 'down' ? `${100 - height}vh` : `${height - 100}vh` : '100vh',
+  }
+
   return (
-    <div className={s.orange}></div>
+    <div className={s.orange} style={styles}></div>
   );
 }

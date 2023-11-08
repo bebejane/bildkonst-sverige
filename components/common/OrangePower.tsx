e81c89016d808024d246f3063d3d7bd2c3b2c6de
Bebe Jane <bjornthief@gmail.com>
2023-11-08 16:58:41 +0100

    layots

--- components/common/OrangePower.tsx
+++ components/common/OrangePower.tsx
@@ -1,29 +1,15 @@
 'use client'
 
 import s from './OrangePower.module.scss'
-import cn from 'classnames'
-import { is } from 'date-fns/locale';
-import { useScrollInfo } from 'dato-nextjs-utils/hooks'
 import { usePathname } from 'next/navigation';
-import path from 'path';
-import { useEffect, useRef, useState } from 'react';
-import { useScrollData } from "scroll-data-hook";
-
-
-function isElementInViewport(el: HTMLElement) {
-  return el.getBoundingClientRect()?.top <= (window.innerHeight || document.documentElement.clientHeight)
-
-}
+import { useEffect } from 'react';
 
 export default function OrangePower({ }: {}) {
 
   const pathname = usePathname()
 
   useEffect(() => {
-    //return
-
     const paragraphs = Array.from(document.querySelectorAll('p')).filter(p => !isElementInViewport(p))
-
     const observePargraphs = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -32,11 +18,7 @@ export default function OrangePower({ }: {}) {
           entry.target.classList.add(s.active)
         }
       })
-    }, {
-      threshold: 0.2,
-      rootMargin: '0px 0px -20% 0px'
-
-    })
+    }, { threshold: 0.2, rootMargin: '0px 0px -20% 0px' })
 
     paragraphs.forEach(p => {
       p.classList.add(s.paragraph)
@@ -48,30 +30,9 @@ export default function OrangePower({ }: {}) {
     }
   }, [pathname])
 
-
-
   return null
-  /*
-  const [active, setActive] = useState(false)
-  const stylesRef = useRef<any>({})
-  const {
-    scrolling,
-    time,
-    speed,
-    direction,
-    position,
-    relativeDistance,
-    totalDistance
-  } = useScrollData({
-    onScrollStart: () => setActive(true),
-    onScrollEnd: () => setActive(false)
-  });
+}
 
-  const height = Math.floor((speed.y / 10000) * 50)
-  stylesRef.current = direction.y === 'down' ? { top: `${90 - height}vh` } : direction.y === 'up' ? { top: '100vh' } : stylesRef.current
-
-  return (
-    <div className={cn(s.orange, active && s.active)} style={stylesRef.current}></div>
-  );
-  */
+function isElementInViewport(el: HTMLElement) {
+  return el.getBoundingClientRect()?.top <= (window.innerHeight || document.documentElement.clientHeight)
 }

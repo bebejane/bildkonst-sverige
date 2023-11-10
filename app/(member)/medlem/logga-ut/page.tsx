2b3dbb749096e0e6e67b42989e43096107bc14e7
Bebe Jane <bjornthief@gmail.com>
2023-11-10 16:35:53 +0100

    fix logout

--- app/(member)/medlem/logga-ut/page.tsx
+++ app/(member)/medlem/logga-ut/page.tsx
@@ -3,15 +3,21 @@
 import s from './page.module.scss'
 import { useEffect } from 'react'
 import { signOut } from 'next-auth/react'
+import { sleep } from 'dato-nextjs-utils/utils'
 
 export default function Logout() {
 
   useEffect(() => {
-    signOut({ callbackUrl: '/' }).then(() => {
-      console.log('Sign out success')
-    }).catch((error) => {
-      console.error('Sign out error', error)
-    })
+    const logout = async () => {
+      await sleep(500)
+      signOut({ callbackUrl: '/' }).then(() => {
+        console.log('Sign out success')
+      }).catch((error) => {
+        console.error('Sign out error', error)
+      })
+    }
+    logout()
+
   }, [])
 
   return (
--- components/blocks/TextBlock.tsx
+++ components/blocks/TextBlock.tsx
@@ -10,24 +10,32 @@ type Props = {
 export default async function TextBlock({ data: { texts } }: Props) {
 
   const links = await Promise.all(texts.filter(({ link }) => link).map(async ({ link }) => ({
+    __typename: link.__typename,
     id: link.id,
     slug: link.__typename === 'InternalLinkRecord' ? await recordToRoute(link.link) : link?.url
   })))
-  const columns = texts.length === 1 ? s.one : texts.length === 2 ? s.two : s.three
+
+  const columns = texts.length
+  const columnsClass = columns === 1 ? s.one : columns === 2 ? s.two : s.three
+  const Header = ({ children }) => columns < 3 ? <h2>{children}</h2> : <h3>{children}</h3>
 
   return (
-    <div className={cn(s.container, columns)}>
+    <div className={cn(s.container, columnsClass)}>
       {texts.map(({ headline, text, link }, i) => {
-        const slug = links.find(({ id }) => id === link?.id)?.slug
-        return link && slug ?
-          <Link key={i} href={slug} className={s.block}>
-            <h3>{headline}</h3>
+        const l = links.find(({ id }) => id === link?.id)
+        return l?.slug ?
+          <Link
+            key={i} href={l.slug}
+            className={s.block}
+            target={l.__typename === 'ExternalLinkRecord' ? "_blank" : undefined}
+          >
+            <Header>{headline}</Header>
             <p>{text}</p>
           </Link>
           :
           <div key={i} className={s.block}>
-            <h3>{headline}</h3>
-            {text}
+            <Header>{headline}</Header>
+            <p>{text}</p>
           </div>
       })}
     </div>

b7129e8005d0ca96542e8d00e7e05ad732b75ccd
Bebe Jane <bjornthief@gmail.com>
2023-11-10 14:37:12 +0100

    newsletter

--- components/nav/Footer.tsx
+++ components/nav/Footer.tsx
@@ -1,14 +1,38 @@
-'use server'
+'use client'
 
 import Link from "next/link";
 import s from './Footer.module.scss'
+import cn from 'classnames'
+import NewsletterForm from "@components/common/NewsLetterForm";
+import { useEffect, useState } from "react";
 
-export default async function Footer({ }: {}) {
+export default function Footer({ }: {}) {
+  const [showNewsletter, setShowNewsletter] = useState(false)
+
+  useEffect(() => {
+    document.body.classList.toggle('slide-up', showNewsletter)
+  }, [showNewsletter])
+
+
   return (
-    <footer className={s.footer}>
-      <div><span>© 2023 Bildkonst Sverige</span></div>
-      <h1 className="logo"><Link href={'/'}>Bildkonst<br />sverige</Link></h1>
-      <div><span>Nyhetsbrev</span><span>Kontakt</span></div>
-    </footer>
+    <>
+      <footer className={s.footer}>
+        <div><span>© 2023 Bildkonst Sverige</span></div>
+        <h1 className="logo">
+          <Link href={'/'}>Bildkonst<br />sverige</Link>
+        </h1>
+        <div>
+          <span onClick={() => setShowNewsletter(!showNewsletter)}>
+            Nyhetsbrev
+          </span>
+          <span><Link href="/kontakt">Kontakt</Link></span>
+        </div>
+      </footer>
+      <div className={cn(s.newsletter, showNewsletter && s.show)}>
+        <h3>Prenumerera på vårt nyhetsbrev</h3>
+        <NewsletterForm />
+        <button className={s.close} onClick={() => setShowNewsletter(false)}>Close</button>
+      </div>
+    </>
   );
 }

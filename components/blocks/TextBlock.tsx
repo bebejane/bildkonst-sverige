294538e851b3e7df87de7d845521377ea3375601
Bebe Jane <bjornthief@gmail.com>
2023-11-10 13:16:24 +0100

    blocks

--- components/blocks/TextBlock.tsx
+++ components/blocks/TextBlock.tsx
@@ -1,16 +1,29 @@
-import s from './PeopleCollectionBlock.module.scss'
+import s from './TextBlock.module.scss'
 import cn from 'classnames'
-import { Image } from "react-datocms"
+import { recordToRoute } from '@lib/routes'
+import Link from 'next/link'
 
 type Props = {
   data: TextBlockRecord
 }
 
 export default function TextBlock({ data: { id, texts } }: Props) {
-  console.log(texts)
+  const columns = texts.length === 1 ? s.one : texts.length === 2 ? s.two : s.three
   return (
-    <div className={s.container}>
-      Text block!
-    </div>
+    <section className={cn(s.container, columns)}>
+      {texts.map(async ({ headline, text, link }, i) => {
+        const slug = link?.__typename === 'InternalLinkRecord' ? await recordToRoute(link.link) : link?.url
+        return link ?
+          <Link key={id} href={slug} className={s.block}>
+            <h3>{headline}</h3>
+            {text}
+          </Link>
+          :
+          <div key={i} className={s.block}>
+            <h3>{headline}</h3>
+            {text}
+          </div>
+      })}
+    </section>
   )
 }

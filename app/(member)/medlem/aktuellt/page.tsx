e7db5cfc4ed210d4d9c9b5c042d792fbd6197f07
Bebe Jane <bjornthief@gmail.com>
2023-12-08 11:48:58 +0100

    fix

--- app/(member)/medlem/aktuellt/page.tsx
+++ app/(member)/medlem/aktuellt/page.tsx
@@ -28,38 +28,45 @@ export default async function News() {
     <article className={s.container}>
       <h3>Aktuellt för medlemmar</h3>
       <div className="grid">
-
         <ul className={s.main}>
-          {extendedNews.map(({ image, slug, title, intro, _publishedAt, category }) => (
+          {extendedNews.map(({ image, slug, title, intro, _publishedAt, category, externalUrl }) => (
             <li key={slug}>
               <Link href={`/medlem/aktuellt/${slug}`}>
-                <figure>
-                  <Image data={image.responsiveImage} />
-                </figure>
+                {image &&
+                  <figure>
+                    <Image data={image.responsiveImage} />
+                  </figure>
+                }
                 <h2>{title}</h2>
                 <span className="date">{category?.title} • {format(new Date(_publishedAt), 'yyyy-MM-dd')}</span>
-
                 <StructuredContent className="intro" content={intro} />
+                {externalUrl &&
+                  <a href={externalUrl} target="_blank" rel="noreferrer">Läs mer</a>
+                }
               </Link>
             </li>
           ))}
         </ul>
         <ul className={s.left}>
-          {shortNews.map(({ image, slug, title, intro, _publishedAt, category }) => (
+          {shortNews.map(({ image, slug, title, intro, _publishedAt, category, externalUrl }) => (
             <li key={slug}>
-              <figure>
-                <Image data={image.responsiveImage} />
-              </figure>
+              {image &&
+                <figure>
+                  <Image data={image.responsiveImage} />
+                </figure>
+              }
               <span className="date">{category?.title} • {format(new Date(_publishedAt), 'yyyy-MM-dd')}<br />
               </span>
               <h4>{title}</h4>
-
               <StructuredContent className="small" content={intro} />
+              {externalUrl &&
+                <a href={externalUrl} target="_blank" rel="noreferrer">Läs mer</a>
+              }
             </li>
           ))}
         </ul>
       </div>
-    </article >
+    </article>
   );
 }
 

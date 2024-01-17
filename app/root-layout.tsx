4629b1888778a7e4c8a220674c7b8d301e44defc
Bebe Jane <bjornthief@gmail.com>
2024-01-17 12:19:01 +0100

    menu background

--- app/root-layout.tsx
+++ app/root-layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({ children, backgroundColor }: LayoutPr
   return (
     <html lang="sv">
       <body id="root" style={{ backgroundColor }}>
-        <NavBar menu={menu} />
+        <NavBar menu={menu} backgroundColor={backgroundColor} />
         <main>
           {children}
         </main>

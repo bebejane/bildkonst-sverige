36d6246d52f5407b7c8035b960f46a0a46b1a1c7
Bebe Jane <bjornthief@gmail.com>
2024-01-22 16:42:40 +0100

    filter

--- app/(member)/medlem/resurser/page.tsx
+++ app/(member)/medlem/resurser/page.tsx
@@ -29,6 +29,12 @@ export default async function Resources({ searchParams }) {
   const themes: string[] = searchParams.tema ? searchParams.tema.split(',') : []
   const filterResources = allResources.filter(({ theme }) => themes.length === 0 || theme.some(({ title }) => themes.includes(title)))
 
+  const sortByAuthor = (a: AllResourcesQuery['allResources'][0], b: AllResourcesQuery['allResources'][0]) => {
+    if (!a.author && b.author) return 1
+    if (a.author && !b.author) return -1
+    return a.author === b.author ? 0 : a.author > b.author ? 1 : -1
+  }
+
   return (
     <article className={s.container}>
       <h3>
@@ -80,9 +86,9 @@ export default async function Resources({ searchParams }) {
           :
           <table className={cn(s.list)}>
             <tbody>
-              {filterResources.sort((a, b) => !a.author ? 1 : a.author > b.author ? 1 : -1).map(({ id, link, title, subtitle, author, publisher, category, theme }) => (
+              {filterResources.sort(sortByAuthor).map(({ id, link, title, subtitle, author, category, theme }) => (
                 <tr key={id}>
-                  <td><h4><Link href={link.url} target="new">{[author].filter(s => s).join(', ')}</Link></h4></td>
+                  <td><h4><Link href={link.url} target="new">{author}</Link></h4></td>
                   <td><h4><Link href={link.url} target="new">{title}{subtitle && <> — {subtitle}</>}</Link></h4></td>
                   <td className="date"><Link href={link.url} target="new">{category?.title}</Link></td>
                   <td className="date"><Link href={link.url} target="new">{theme.map(({ title }) => title).join(', ')}</Link></td>

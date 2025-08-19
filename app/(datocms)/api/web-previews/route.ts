a7508e68b82307489056abd942dcbf60b443d6d4
Bebe Jane <bjornthief@gmail.com>
2025-08-19 15:22:40 +0200

    fix 2

--- app/(datocms)/api/web-previews/route.ts
+++ app/(datocms)/api/web-previews/route.ts
@@ -1,24 +1,23 @@
-import { webPreviews, cors } from 'next-dato-utils/route-handlers'
+import { webPreviews, cors } from 'next-dato-utils/route-handlers';
 import { buildRoute } from '@lib/routes';
 import { NextRequest } from 'next/server';
 
-export const runtime = "edge"
-export const dynamic = 'force-dynamic'
+export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
 
 export async function POST(req: NextRequest) {
-  return await webPreviews(req, async ({ item, itemType }) => {
-    const path = await buildRoute(itemType.attributes.api_key, item.attributes)
-    if (!path) return null
-    return `${path}?secret=${process.env.DATOCMS_PREVIEW_SECRET}`
-  })
+	return await webPreviews(req, async ({ item, itemType }) => {
+		const paths = await buildRoute(itemType.attributes.api_key, item.attributes);
+		if (!paths) return null;
+		return `${paths[0]}?secret=${process.env.DATOCMS_PREVIEW_SECRET}`;
+	});
 }
 
 export async function OPTIONS(req: Request) {
-
-  return await cors(req, new Response('ok', { status: 200 }), {
-    origin: '*',
-    methods: ['POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    preflightContinue: false
-  })
+	return await cors(req, new Response('ok', { status: 200 }), {
+		origin: '*',
+		methods: ['POST', 'OPTIONS'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+		preflightContinue: false,
+	});
 }
